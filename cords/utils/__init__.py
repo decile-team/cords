438b9c9e61bf3edf21f0ff7058cda2f6513f1797
Krishnateja Killamsetty <krishnateja.killamsetty@utdallas.edu>
2021-01-15 17:39:45 -0600

    bug fixes in setup.py

--- cords/utils/__init__.py
+++ cords/utils/__init__.py
@@ -4,8 +4,4 @@
 
 from .custom_dataset import CustomDataset
 from .custom_dataset import load_dataset_custom
-<<<<<<< HEAD
 
-
-=======
->>>>>>> 75adb7bdcf37776a3e3a8d9bb671840a639072a8
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setup(
     name='cords',
-    version='0.1.2',
+    version='0.1.3',
     author='Krishnateja Killamsetty, Dheeraj Bhat, Rishabh Iyer',
     author_email='krishnatejakillamsetty@gmail.com',
     #packages=['cords', 'cords/selectionstrategies', 'cords/utils'],
