63ef4596d6450324e11e0f5b6e8c83fb5d45737a
Krishnateja Killamsetty <krishnateja.killamsetty@utdallas.edu>
2021-01-15 21:07:51 -0600

    Updated Setup File

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setup(
     name='cords',
-    version='0.1.3',
+    version='0.1.4',
     author='Krishnateja Killamsetty, Dheeraj Bhat, Rishabh Iyer',
     author_email='krishnatejakillamsetty@gmail.com',
     #packages=['cords', 'cords/selectionstrategies', 'cords/utils'],
