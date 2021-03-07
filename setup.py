ebcb512969846619e211962c5e181bdbea492af8
Krishnateja Killamsetty <krishnatejaiit@gmail.com>
2021-03-07 14:18:00 -0600

    Random Online config

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setup(
     name='cords',
-    version='0.2.4',
+    version='0.2.3',
     author='Krishnateja Killamsetty, Dheeraj Bhat, Rishabh Iyer',
     author_email='krishnatejakillamsetty@gmail.com',
     #packages=['cords', 'cords/selectionstrategies', 'cords/utils'],
