05fd3f482951333d4af172d38c925527ef2d52e2
Dheeraj Bhat <dheerajnbhat@gmail.com>
2021-03-12 12:20:38 +0530

    Update mnist_net.py

--- cords/utils/models/mnist_net.py
+++ cords/utils/models/mnist_net.py
@@ -16,17 +16,30 @@ class MnistNet(nn.Module):
         self.fc2 = nn.Linear(128, 10)
 
 
-    def forward(self, x, last=False):
-        out = self.conv1(x)
-        out = F.relu(out)
-        out = self.conv2(out)
-        out = F.relu(out)
-        out = F.max_pool2d(out, 2)
-        out = self.dropout1(out)
-        out = torch.flatten(out, 1)
-        out = self.fc1(out)
-        out = F.relu(out)
-        e = self.dropout2(out)
+    def forward(self, x, last=False, freeze=False):
+        if freeze:
+            with torch.no_grad():
+                out = self.conv1(x)
+                out = F.relu(out)
+                out = self.conv2(out)
+                out = F.relu(out)
+                out = F.max_pool2d(out, 2)
+                out = self.dropout1(out)
+                out = torch.flatten(out, 1)
+                out = self.fc1(out)
+                out = F.relu(out)
+                e = self.dropout2(out) 
+        else:
+            out = self.conv1(x)
+            out = F.relu(out)
+            out = self.conv2(out)
+            out = F.relu(out)
+            out = F.max_pool2d(out, 2)
+            out = self.dropout1(out)
+            out = torch.flatten(out, 1)
+            out = self.fc1(out)
+            out = F.relu(out)
+            e = self.dropout2(out)
         out = self.fc2(e)
         if last:
             return out, e
