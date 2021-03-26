8e7f9f1d840988cde6e0b5035ab6f9ba404f5485
krishnatejakk <krishnatejakillamsetty@gmail.com>
2021-03-25 23:06:34 -0500

    MobileNetV2 Version Test

--- cords/utils/models/mobilenetv2tf.py
+++ cords/utils/models/mobilenetv2tf.py
@@ -1,192 +1,129 @@
-from collections import OrderedDict
-
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.nn import init
 
 
-def _make_divisible(v, divisor, min_value=None):
-    """
-    This function is taken from the original tf repo.
-    It ensures that all layers have a channel number that is divisible by 8
-    It can be seen here:
-    https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet.py
-    :param v:
-    :param divisor:
-    :param min_value:
-    :return:
-    """
-    if min_value is None:
-        min_value = divisor
-    new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
-    # Make sure that round down does not go down by more than 10%.
-    if new_v < 0.9 * v:
-        new_v += divisor
-    return new_v
+class BaseBlock(nn.Module):
+    alpha = 1
 
+    def __init__(self, input_channel, output_channel, t=6, downsample=False):
+        """
+            t:  expansion factor, t*input_channel is channel of expansion layer
+            alpha:  width multiplier, to get thinner models
+            rho:    resolution multiplier, to get reduced representation
+        """
+        super(BaseBlock, self).__init__()
+        self.stride = 2 if downsample else 1
+        self.downsample = downsample
+        self.shortcut = (not downsample) and (input_channel == output_channel)
 
-class LinearBottleneck(nn.Module):
-    def __init__(self, inplanes, outplanes, stride=1, t=6, activation=nn.ReLU6):
-        super(LinearBottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(inplanes, inplanes * t, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(inplanes * t)
-        self.conv2 = nn.Conv2d(inplanes * t, inplanes * t, kernel_size=3, stride=stride, padding=1, bias=False,
-                               groups=inplanes * t)
-        self.bn2 = nn.BatchNorm2d(inplanes * t)
-        self.conv3 = nn.Conv2d(inplanes * t, outplanes, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(outplanes)
-        self.activation = activation(inplace=True)
-        self.stride = stride
-        self.t = t
-        self.inplanes = inplanes
-        self.outplanes = outplanes
+        # apply alpha
+        input_channel = int(self.alpha * input_channel)
+        output_channel = int(self.alpha * output_channel)
 
-    def forward(self, x):
-        residual = x
+        # for main path:
+        c = t * input_channel
+        # 1x1   point wise conv
+        self.conv1 = nn.Conv2d(input_channel, c, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(c)
+        # 3x3   depth wise conv
+        self.conv2 = nn.Conv2d(c, c, kernel_size=3, stride=self.stride, padding=1, groups=c, bias=False)
+        self.bn2 = nn.BatchNorm2d(c)
+        # 1x1   point wise conv
+        self.conv3 = nn.Conv2d(c, output_channel, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(output_channel)
 
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.activation(out)
+    def forward(self, inputs):
+        # main path
+        x = F.relu6(self.bn1(self.conv1(inputs)), inplace=True)
+        x = F.relu6(self.bn2(self.conv2(x)), inplace=True)
+        x = self.bn3(self.conv3(x))
 
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.activation(out)
+        # shortcut path
+        x = x + inputs if self.shortcut else x
 
-        out = self.conv3(out)
-        out = self.bn3(out)
-
-        if self.stride == 1 and self.inplanes == self.outplanes:
-            out += residual
-
-        return out
-
+        return x
 
 class MobileNet2(nn.Module):
-    """MobileNet2 implementation.
-    """
-
-    def __init__(self, scale=1.0, input_size=224, t=6, in_channels=3, num_classes=1000, activation=nn.ReLU6):
-        """
-        MobileNet2 constructor.
-        :param in_channels: (int, optional): number of channels in the input tensor.
-                Default is 3 for RGB image inputs.
-        :param input_size:
-        :param num_classes: number of classes to predict. Default
-                is 1000 for ImageNet.
-        :param scale:
-        :param t:
-        :param activation:
-        """
-
+    def __init__(self, output_size, alpha=1):
         super(MobileNet2, self).__init__()
+        self.output_size = output_size
 
-        self.scale = scale
-        self.t = t
-        self.activation_type = activation
-        self.activation = activation(inplace=True)
-        self.num_classes = num_classes
+        # first conv layer 
+        self.conv0 = nn.Conv2d(3, int(32 * alpha), kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn0 = nn.BatchNorm2d(int(32 * alpha))
 
-        self.num_of_channels = [32, 16, 24, 32, 64, 96, 160, 320]
-        # assert (input_size % 32 == 0)
+        # build bottlenecks
+        BaseBlock.alpha = alpha
+        self.bottlenecks = nn.Sequential(
+            BaseBlock(32, 16, t=1, downsample=False),
+            BaseBlock(16, 24, downsample=False),
+            BaseBlock(24, 24),
+            BaseBlock(24, 32, downsample=False),
+            BaseBlock(32, 32),
+            BaseBlock(32, 32),
+            BaseBlock(32, 64, downsample=True),
+            BaseBlock(64, 64),
+            BaseBlock(64, 64),
+            BaseBlock(64, 64),
+            BaseBlock(64, 96, downsample=False),
+            BaseBlock(96, 96),
+            BaseBlock(96, 96),
+            BaseBlock(96, 160, downsample=True),
+            BaseBlock(160, 160),
+            BaseBlock(160, 160),
+            BaseBlock(160, 320, downsample=False))
 
-        self.c = [_make_divisible(ch * self.scale, 8) for ch in self.num_of_channels]
-        self.n = [1, 1, 2, 3, 4, 3, 3, 1]
-        self.s = [2, 1, 2, 2, 2, 1, 2, 1]
-        self.conv1 = nn.Conv2d(in_channels, self.c[0], kernel_size=3, bias=False, stride=self.s[0], padding=1)
-        self.bn1 = nn.BatchNorm2d(self.c[0])
-        self.bottlenecks = self._make_bottlenecks()
+        # last conv layers and fc layer
+        self.conv1 = nn.Conv2d(int(320 * alpha), 1280, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(1280)
+        self.fc = nn.Linear(1280, output_size)
+        self.embDim = 1280
+        # weights init
+        self.weights_init()
 
-        # Last convolution has 1280 output channels for scale <= 1
-        self.last_conv_out_ch = 1280 if self.scale <= 1 else _make_divisible(1280 * self.scale, 8)
-        self.conv_last = nn.Conv2d(self.c[-1], self.last_conv_out_ch, kernel_size=1, bias=False)
-        self.bn_last = nn.BatchNorm2d(self.last_conv_out_ch)
-        self.avgpool = nn.AdaptiveAvgPool2d(1)
-        self.dropout = nn.Dropout(p=0.2, inplace=True)  # confirmed by paper authors
-        self.embDim = self.last_conv_out_ch
-        self.fc = nn.Linear(self.last_conv_out_ch, self.num_classes)
-        self.init_params()
-
-    def init_params(self):
+    def weights_init(self):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                init.kaiming_normal_(m.weight, mode='fan_out')
-                if m.bias is not None:
-                    init.constant_(m.bias, 0)
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+
             elif isinstance(m, nn.BatchNorm2d):
-                init.constant_(m.weight, 1)
-                init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                init.normal_(m.weight, std=0.001)
-                if m.bias is not None:
-                    init.constant_(m.bias, 0)
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
 
-    def _make_stage(self, inplanes, outplanes, n, stride, t, stage):
-        modules = OrderedDict()
-        stage_name = "LinearBottleneck{}".format(stage)
+    def forward(self, inputs, last=False, freeze=False):
 
-        # First module is the only one utilizing stride
-        first_module = LinearBottleneck(inplanes=inplanes, outplanes=outplanes, stride=stride, t=t,
-                                        activation=self.activation_type)
-        modules[stage_name + "_0"] = first_module
-
-        # add more LinearBottleneck depending on number of repeats
-        for i in range(n - 1):
-            name = stage_name + "_{}".format(i + 1)
-            module = LinearBottleneck(inplanes=outplanes, outplanes=outplanes, stride=1, t=6,
-                                      activation=self.activation_type)
-            modules[name] = module
-
-        return nn.Sequential(modules)
-
-    def _make_bottlenecks(self):
-        modules = OrderedDict()
-        stage_name = "Bottlenecks"
-
-        # First module is the only one with t=1
-        bottleneck1 = self._make_stage(inplanes=self.c[0], outplanes=self.c[1], n=self.n[1], stride=self.s[1], t=1,
-                                       stage=0)
-        modules[stage_name + "_0"] = bottleneck1
-
-        # add more LinearBottleneck depending on number of repeats
-        for i in range(1, len(self.c) - 1):
-            name = stage_name + "_{}".format(i)
-            module = self._make_stage(inplanes=self.c[i], outplanes=self.c[i + 1], n=self.n[i + 1],
-                                      stride=self.s[i + 1],
-                                      t=self.t, stage=i)
-            modules[name] = module
-
-        return nn.Sequential(modules)
-
-    def forward(self, x, last=False, freeze=False):
         if freeze:
             with torch.no_grad():
-                x = self.conv1(x)
-                x = self.bn1(x)
-                x = self.activation(x)
+                # first conv layer
+                x = F.relu6(self.bn0(self.conv0(inputs)), inplace=True)
+                # assert x.shape[1:] == torch.Size([32, 32, 32])
+                # bottlenecks
                 x = self.bottlenecks(x)
-                x = self.conv_last(x)
-                x = self.bn_last(x)
-                x = self.activation(x)
-                # average pooling layer
-                x = self.avgpool(x)
-                x = self.dropout(x)
-                # flatten for input to fully-connected layer
-                e = x.view(x.size(0), -1)
+                # assert x.shape[1:] == torch.Size([320, 8, 8])
+                # last conv layer
+                x = F.relu6(self.bn1(self.conv1(x)), inplace=True)
+                # assert x.shape[1:] == torch.Size([1280,8,8])
+                # global pooling and fc (in place of conv 1x1 in paper)
+                x = F.adaptive_avg_pool2d(x, 1)
+                e = x.view(x.shape[0], -1)
         else:
-            x = self.conv1(x)
-            x = self.bn1(x)
-            x = self.activation(x)
+            # first conv layer
+            x = F.relu6(self.bn0(self.conv0(inputs)), inplace=True)
+            # assert x.shape[1:] == torch.Size([32, 32, 32])
+            # bottlenecks
             x = self.bottlenecks(x)
-            x = self.conv_last(x)
-            x = self.bn_last(x)
-            x = self.activation(x)
-            # average pooling layer
-            x = self.avgpool(x)
-            x = self.dropout(x)
-            # flatten for input to fully-connected layer
-            e = x.view(x.size(0), -1)
+            # assert x.shape[1:] == torch.Size([320, 8, 8])
+            # last conv layer
+            x = F.relu6(self.bn1(self.conv1(x)), inplace=True)
+            # assert x.shape[1:] == torch.Size([1280,8,8])
+            # global pooling and fc (in place of conv 1x1 in paper)
+            x = F.adaptive_avg_pool2d(x, 1)
+            e = x.view(x.shape[0], -1)
         x = self.fc(e)
+
         if last:
             return x, e
         else:
@@ -194,24 +131,3 @@ class MobileNet2(nn.Module):
 
     def get_embedding_dim(self):
         return self.embDim
-
-
-if __name__ == "__main__":
-    """Testing
-    """
-    model1 = MobileNet2()
-    print(model1)
-    model2 = MobileNet2(scale=0.35)
-    print(model2)
-    model3 = MobileNet2(in_channels=2, num_classes=10)
-    print(model3)
-    x = torch.randn(1, 2, 224, 224)
-    print(model3(x))
-    model4_size = 32 * 10
-    model4 = MobileNet2(input_size=model4_size, num_classes=10)
-    print(model4)
-    x2 = torch.randn(1, 3, model4_size, model4_size)
-    print(model4(x2))
-    model5 = MobileNet2(input_size=196, num_classes=10)
-    x3 = torch.randn(1, 3, 196, 196)
-    print(model5(x3))  # fail
--- train.py
+++ train.py
@@ -54,7 +54,7 @@ class TrainClassifier:
         elif self.configdata['model']['architecture'] == 'MobileNetV2':
             model = MobileNetV2(self.configdata['model']['numclasses'])
         elif self.configdata['model']['architecture'] == 'MobileNet2':
-            model = MobileNet2(input_size=32, num_classes=self.configdata['model']['numclasses'])
+            model = MobileNet2(output_size=self.configdata['model']['numclasses'])
         model = model.to(self.configdata['train_args']['device'])
         return model
 
