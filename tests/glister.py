c6ab42fe5cc7bb30656ac667593c408c3f84fc11
noil-reed <ping69852@gmail.com>
2021-06-29 21:08:02 -0500

    dual tests

---
+++ tests/glister.py
@@ -0,0 +1,17 @@
+# Sanity checks for glister
+
+
+def grads_val():
+    pass
+
+
+def grads_train():
+    pass
+
+
+def taylor_approx():
+    pass
+
+
+def select():
+    pass
---
+++ tests/integration.py
@@ -0,0 +1,30 @@
+# test if the whole workflow is executable
+
+
+
+def craig():
+    pass
+
+
+def data_selection():
+    pass
+
+
+def fixed_weight():
+    pass
+
+
+def glister():
+    pass
+
+
+def omp_gradmatch():
+    pass
+
+
+def random():
+    pass
+
+
+def submodular_selection():
+    pass
