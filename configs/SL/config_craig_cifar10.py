a307ca59d486865235963c3db36f706d5a8460e2
krishnatejakk <krishnateja.killamsetty@utdallas.edu>
2022-02-25 21:44:02 -0800

    if_convex argument for CRAIG Strategy

--- configs/SL/config_craig_cifar10.py
+++ configs/SL/config_craig_cifar10.py
@@ -37,7 +37,8 @@ config = dict(setting="SL",
                                 kappa=0,
                                 linear_layer=False,
                                 optimizer='lazy',
-                                selection_type='PerClass'
+                                selection_type='PerClass',
+                                if_convex=False
                                 ),
 
               train_args=dict(num_epochs=300,
