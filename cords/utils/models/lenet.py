53804ec0fd31187dbd6c94c09721d3b099eeffc2
Aakriti <aakriti281020@gmail.com>
2022-05-19 09:02:06 +0530

    Update lenet.py

--- cords/utils/models/lenet.py
+++ cords/utils/models/lenet.py
@@ -12,7 +12,7 @@ class LeNet(nn.Module):
         
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1   = nn.Linear(16*5*5, 120)
+        self.fc1   = nn.Linear(256, 120)
         self.fc2   = nn.Linear(120, 84)
         self.fc3   = nn.Linear(84, 10)
 
